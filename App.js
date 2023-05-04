89d5ffb033e41781c6b52b654e42b8aa3c036d07
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-05-04 10:45:45 +0100

    finish Js

--- App.js
+++ App.js
@@ -6,10 +6,12 @@ let isAlive = true
 let message = "" 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-
+let cardsEl = document.getElementById("cards-el")
 
 
 function startGame() {
+
+cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
 
 sumEl.textContent = "Sum: " + sum
 
@@ -33,7 +35,13 @@ messageEl.textContent = message
 
 }
 
+function newCard() {
 
+
+
+
+    
+}
 
 
 
