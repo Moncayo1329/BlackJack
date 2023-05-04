8253c5e4af4eb5e30b8c3e4fb4d1265ba281dba3
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-05-04 02:05:02 +0100

    testing

---
+++ App.js
@@ -0,0 +1,39 @@
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard 
+let hasBlackJack = false 
+let isAlive = true 
+let message = "" 
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+
+
+
+function startGame() {
+
+sumEl.textContent = "Sum: " + sum
+
+
+    if(sum <= 20 ) {
+ 
+        message = "Do you want to draw a new card ?"
+      } 
+       else if ( sum === 21 ){  
+          message = "wow! bitch Blackjack!"
+       hasBlackJack = true 
+      
+      } else {  
+          message = "Fuck off you are out!"
+          
+          isAlive = false
+      } 
+
+
+messageEl.textContent = message
+
+}
+
+
+
+
+
